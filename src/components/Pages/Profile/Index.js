a6b8f9948166f62452a2101968ee8ef25e9ecb73
Rocky Gonzales <gonzrock12@gmail.com>
2022-02-10 13:23:28 +0800

    Update Index.js

--- src/components/Pages/Profile/Index.js
+++ src/components/Pages/Profile/Index.js
@@ -48,7 +48,7 @@ class Index extends React.Component {
                         <span>&nbsp; • &nbsp;</span>
                         <button className="mx-3 uppercase">Achievements</button>
                     </div> */}
-                    <div className="flex justify-between border-b pb-5 shadow-border_shadow bg-white_color p-5">
+                    <div className="block lg:flex justify-between border-b pb-5 shadow-border_shadow bg-white_color p-5">
                         <div className="flex">
                             <div className="half-circle relative">
                                 <div className="rounded-full absolute ml-1 w-28 mt-1">
@@ -67,7 +67,7 @@ class Index extends React.Component {
                                 </div>
                             </div>
                         </div>
-                        <div className="flex pt-7">
+                        <div className="flex pt-7 justify-center">
                             <div className="mx-5">
                                 <div className="font-bold">Following</div>
                                 <div className="text-right text-2xl">0</div>
@@ -82,23 +82,23 @@ class Index extends React.Component {
                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
                                         </svg>
-                                        Add Friends
+                                        <span className="hidden sm:block">Add Friends</span>
                                     </button>
                                 </div>
                                 <div className="mt-2">
-                                    <button onClick={this.handleOpenEditProfileModal} className="text-sm flex bg-vici_secondary w-full text-white_color px-3 py-1 rounded">
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                                    <button onClick={this.handleOpenEditProfileModal} className="text-sm flex bg-vici_secondary w-full text-white_color px-3 py-1 pl-3 rounded">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-0 sm:mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                                         </svg>
-                                        Edit Profile
+                                        <span className="hidden sm:block">Edit Profile</span>
                                     </button>
                                     {this.state.openEditProfileModal && <EditProfile closeModal={this.handleCloseEditProfileModal } />}
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="flex px-24 py-5 bg-white_color justify-between">
-                        <div className="flex">
+                    <div className="block lg:flex lg:px-24 px-5 lg:py-0 py-5 bg-white_color justify-between">
+                        <div className="flex py-5 justify-center">
                             <div className="flex w-52 bg-vici_light_gray rounded-full py-1 px-2">
                                 <img alt="" src="/img/badge-question.png" className="h-10" />
                                 <div>
@@ -114,7 +114,7 @@ class Index extends React.Component {
                                 </div>
                             </div>
                         </div>
-                        <div>
+                        <div className="py-5 flex justify-center">
                             <button className="flex py-2 px-3 border rounded-2xl border-vici_button_txt shadow-border_shadow_button">
                                 <img alt="" src="/img/Frame 1989.png" />
                                 <div className="px-2 pt-1 text-vici_button_txt">New Challenge</div>
@@ -122,14 +122,14 @@ class Index extends React.Component {
                         </div>
                     </div>
                     <div className="mt-6">
-                        <div className="flex w-full">
-                            <div className="w-1/4">
+                        <div className="block sm:flex w-full">
+                            <div className="w-full sm:w-1/4">
                                 <AboutMe />
                             </div>
-                            <div className="w-1/2">
+                            <div className="w-full sm:w-1/2">
                                 <ProfileContent />
                             </div>
-                            <div className="w-1/4">
+                            <div className="w-full sm:w-1/4">
                                 <RightSidebar />
                             </div>
                         </div>
