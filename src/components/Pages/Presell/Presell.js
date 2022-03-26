5aa37aef21ca7ca8c316d0ddb39410252e8459b1
Rocky Gonzales <gonzrock12@gmail.com>
2022-03-26 11:54:04 +0800

    presell page what a challenge

--- src/components/Pages/Presell/Presell.js
+++ src/components/Pages/Presell/Presell.js
@@ -9,23 +9,23 @@ class Presell extends React.Component {
                     <img src="/img/Vici Logo.png" width="63" />
                 </div>                
                 <div className="container mx-auto z-10">
-                    <div className="px-6 py-3 mt-12 rounded-3xl text-2xl text-center bg-vici_bg_opacity">
+                    <div className="px-6 py-3 mt-6 rounded-3xl text-xl text-center bg-vici_bg_opacity">
                         <div>Engage with your audience on a new level with a <span className="font-bold">new content type</span></div>
                         <div>that boosts your brand awareness, conversion, customer retention, word-of-mouth, and ultimately <span className="font-bold">revenue</span></div>
                     </div>
                 </div>
                 <div className="bg-no-repeat bg-left-bottom bg-none xl:bg-presell-circle">
                     <div className="container mx-auto">
-                        <div className="mt-24 block xl:flex">
+                        <div className="xl:mt-20 mt-6 block xl:flex">
                             <div className="w-full xl:w-1/2">
-                                <div className="text-60 whitespace-normal leading-tight text-center xl:text-left xl:whitespace-pre">
+                                <div className="text-5xl whitespace-normal leading-tight text-center xl:text-left xl:whitespace-pre">
                                     Quickly create <br/>
                                     and run viral <span className="font-bold">Challenges</span>
                                 </div>
                                 <div className="text-4xl leading-tight mt-6 text-center xl:text-left">
                                     That your audiences will love to participate and invite their friends
                                 </div>
-                                <div className="text-xl font-bold mt-6 text-vici_secondary text-center xl:text-left">
+                                <div className="text-lg font-bold mt-6 text-vici_secondary text-center xl:text-left">
                                     (without hiring an entire marketing department!)   
                                 </div>
                             </div>
@@ -35,15 +35,126 @@ class Presell extends React.Component {
                                 </div>
                             </div>
                         </div>
-                        <div className="flex justify-center text-xl">
-                            <div className="">
-                                <button className="px-6 py-2 mr-2 rounded-lg bg-primary_color shadow-border_shadow_button hover:text-white_color">Get Early Access</button>
-                                <button className="px-12 py-2 ml-2 rounded-lg border hover:text-primary_color">Learn more</button>
+                        <div className="flex -mt-16 justify-center text-2xl">
+                            <div className="block sm:flex">
+                                <div className="mb-3 sm:mb-0">
+                                    <button className="px-6 py-2 mr-0 sm:mr-2 rounded-lg bg-primary_color shadow-border_shadow_button hover:text-white_color">Get Early Access</button>
+                                </div>
+                                <div>
+                                    <button className="px-14 py-2 ml-0 sm:ml-2 bg-white_color rounded-lg border hover:text-primary_color">Learn more</button>
+                                </div>
                             </div>
                         </div>
                     </div>
+                </div>
+                <div className="mt-6">
+                    <div className="container mx-auto">
+                        <div className="block sm:flex pb-6 border-b border-vici_gray">
+                            <div className="flex w-full sm:w-1/2 p-6 border-r border-vici_gray">
+                                <div>
+                                    <img src="/img/zone/presell/Quote Mark.png" className="w-12" />
+                                </div>
+                                <div className="pl-3 pr-6">
+                                    <p>
+                                        Your project idea is truly interesting. As a marketer I see a potential for business to use Vici and for younger generation customers to engage in challenges.
+                                    </p>
+                                    <div className="flex mt-6">
+                                        <div>
+                                            <img src="/img/zone/presell/dr.donatas.png" className="w-18" />
+                                        </div>
+                                        <div className="pl-3">
+                                            <div className="font-bold">Donatas Jonikas, PhD</div>
+                                            <div className="text-sm">Startup Marketing Supervisor</div>
+                                            <div className="text-sm">Author of Startup Evolution Curve</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex w-full sm:w-1/2 p-6">
+                                <div>
+                                    <img src="/img/zone/presell/Quote Mark.png" className="w-16" />
+                                </div>
+                                <div className="pl-3 pr-6">
+                                    <p>
+                                        If companies have this, they will be able to strengthen the connection with customers and increase their loyalty. I can see us creating challenges to encourage students to achieve certificates in exchange for some kinds of gifts.
+                                    </p>
+                                    <div className="flex mt-6">
+                                        <div>
+                                            <img src="/img/zone/presell/Frame 2700.png" className="w-18" />
+                                        </div>
+                                        <div className="pl-3">
+                                            <div className="font-bold">Truong Van Cuong</div>
+                                            <div className="text-sm">Founder - CEO</div>
+                                            <div className="text-sm">KOHI VIET NAM EDUCATION JOINT STOCK COMPANY</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>     
                 </div>   
-                {/* <img src="/img/zone/presell/Oval Copy 5.png" className="absolute top-9 z-0 mt-60" />     */}         
+
+                <div className="mt-12">
+                    <div className="text-center text-4xl font-extrabold">What’s a Challenge?</div>
+                    <div className="container mx-auto">
+                        <div className="flex mt-6">
+                            <div className="w-1/2 flex justify-center">
+                                <img src="/img/zone/presell/2 23.png" />
+                            </div>
+                            <div className="w-1/2 flex justify-center">
+                                <img src="/img/zone/presell/Feature 2.png" />
+                            </div>
+                        </div>
+                        <div className="p-6 text-center w-10/12 mx-auto">
+                            <div>
+                                A challenge consists of a goal, some actions that your audience needss to take, and optionally some kinds of rewards upon completion. 
+                                Challenge is an underused type of content that is highly viral, engaging, fun, and motivational to your audience.
+                            </div>
+                            <div className="block sm:flex mt-6">
+                                <div className="flex sm:block justify-center">
+                                    <img src="/img/zone/presell/Frame 1781.png" />
+                                </div>
+                                <div className="w-full pl-0 sm:pl-6 pt-5 text-center sm:text-left">
+                                    <div className="text-2xl font-extrabold">
+                                        Challenges are used by different types of businesses and organizations to
+                                    </div>
+                                    <div>boost brand awareness, generate leads, drive sales & increase customer engagement</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="mt-12 bg-vici_light_gray p-12">
+                    <div className="container mx-auto">
+                        <div className="block lg:flex">
+                            <div className="w-full lg:w-2/3 lg:px-12 px-0 pb-12 pt-0 xl:pt-28 text-xl">
+                                <p>
+                                    In 2019, Too Faced Cosmetics noticed that one of their lip glosses was selling out due to a viral TikTok challenge where young users were showing off the effects fo the product on their pout with <span className="font-bold">before and after videos.</span>
+                                </p>
+                                <br />
+                                <p className="font-bold">Too Faced didn’t miss this opportunity. They launched a similar challenge to promote a new mascara, and sales skyrocketed.</p>
+                                <br />
+                                <p>
+                                    The strategy worked, generating 1 billion views on the campaign’s hashtag
+                                    #TFDamn Girl up until now.
+                                </p>
+                            </div>
+                            <div className="flex justify-center">
+                                <img src="/img/zone/presell/Frame 2754.png" className="rounded-3xl"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-6 px-12 py-6">
+                    <div className="container mx-auto">
+                        <div className="text-2xl text-center font-bold">
+                            and so many more challenges are used successfully by companies and organizations around the world
+                        </div>
+                    </div>
+                </div>
+
             </div>
         )
     }
